35a55646d8ffa48047dba1d03e1be67f6e3fae60
FerrielMelarpis <fbmelarpis@up.edu.ph>
2022-03-25 18:32:57 +0800

    introduced while loop to traverse the values inside the numbers list

---
+++ hello.rb
@@ -0,0 +1,11 @@
+numbers = [4, 5, 3, -7, 20, 0, 5]
+number = 3
+
+
+index = 0
+while index < numbers.length
+    if numbers[index] == number then
+        puts "Found in array"
+        break
+    end
+end
