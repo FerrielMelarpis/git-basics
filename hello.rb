0042cdd55156be3875fbfe31bfd1c0d99282df9f
FerrielMelarpis <fbmelarpis@up.edu.ph>
2022-03-25 18:50:05 +0800

    optimized my solution

--- hello.rb
+++ hello.rb
@@ -1,16 +1,17 @@
-numbers = [4, 5, 3, -7, 20, 0, 5]
-number = 2
+numbers = [5, 5, 3, -7, 20, 0, 5]
+number = 3
 
 
-index = 0
+index = 1
 while index < numbers.length
     if numbers[index] == number then
         puts "Found in array"
         break
     end
 
-    index = index + 1
-    if index == numbers.length then
-        puts "Not found in array"
-    end
+    index = index + 2
+end
+
+if index == numbers.length then
+    puts "Not found in array"
 end
