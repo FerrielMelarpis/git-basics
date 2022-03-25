24f8b7c13bf4cdd72c46f1ba8064fc2c6babaa54
FerrielMelarpis <fbmelarpis@up.edu.ph>
2022-03-25 19:48:01 +0800

    Revert "Merge branch 'for-loop' into main"
    This reverts commit 878a58c26da3a9a8b1ecd3f9301299c623104662, reversing
    changes made to 5047e4a54b8a3f1ff0eaf49d4b2bfda7e945c109.

--- hello.rb
+++ hello.rb
@@ -1,7 +1,9 @@
 numbers = [5, 5, 3, -7, 20, 0, 5]
 number = 3
 
-for index in (0...numbers.length)
+
+index = 1
+while index < numbers.length
     if numbers[index] == number then
         puts "Found in array"
         break
